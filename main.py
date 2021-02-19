64ce46ba5685d4aac87f6d8e18f53459f12a19ee
Raklit <raklitnewmail@gmail.com>
2021-02-20 00:43:54 +0800

    read data from new file + 2d visualisation

--- main.py
+++ main.py
@@ -8,18 +8,59 @@ import sklearn.decomposition
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+
+import pandas as pd
+
 import numpy as np
 
 import ParserRecord as pr
 
+def color_func_txt(bioms, index):
+    if bioms[index].startswith("water"):
+        return 'b'
+    else:
+        return 'g'
+
+
+def color_func_csv(bioms, index):
+    temp = bioms[index].lower()
+    if "aquatic" in temp or "Marine" in temp or "water" in temp:
+        return 'b'
+    else:
+        return 'g'
+
+def get_data_from_txt():
+    f = open("links.txt", 'r', encoding="utf8")
+    urls, bioms = [], []
+    re_sep = re.compile(r'[\s\t]+')
+    for line in f:
+        temp = re_sep.split(line)
+        urls.append(temp[0])
+        bioms.append(temp[1])
+    f.close()
+    return urls, bioms
+
+def get_data_from_csv():
+    f = pd.read_csv("table.csv")
+    nrows = len(f)
+    urls, bioms = [], []
+    for i in range(nrows):
+        if f["PULDB URL"][i] is np.nan or f["Habitat"][i] is np.nan: continue
+        urls.append(f["PULDB URL"][i])
+        bioms.append(f["Habitat"][i])
+    return urls, bioms
+    
+
 def main():
     args = sys.argv
     browser = webdriver.Firefox()
-    f = open("links.txt", 'r', encoding="utf8")
-    urls = []
-    for line in f:
-        urls.append(line)
-    f.close()
+    # urls, bioms = get_data_from_txt()
+    # color_func = color_func_txt
+    urls, bioms = get_data_from_csv()
+    color_func = color_func_csv
+    if len(urls) == 0:
+        print("Empty url list")
+        return
     records, share_vector, features = [], [], []
     for url in urls:
         temp = pr.ParserRecord()
@@ -40,6 +81,9 @@ def main():
     pca = sklearn.decomposition.PCA(n_components=3)
     pca.fit(features)
     X = pca.transform(features)
+
+    for i in range(len(records)):
+        print(records[i].name, X[i], color_func(bioms, i))
     
     fig = plt.figure(1, figsize=(4, 3))
     plt.clf()
@@ -48,12 +92,9 @@ def main():
     
     for name, label in [(records[i].name, i) for i in range(len(X))]:
         pass
-        # ax.text3D(X[label][0], X[label][1], X[label][2],
-        #     name,
-        #       horizontalalignment='center',
-        #       bbox=dict(alpha=.5, edgecolor='w', facecolor='w'))
+        # ax.text3D(X[label][0], X[label][1], X[label][2] + 0.5, name, horizontalalignment='center', size='x-small',bbox=dict(alpha=.5, edgecolor='w', facecolor='w'))
 
-    ax.scatter(X[:, 0], X[:, 1], X[:, 2], cmap=plt.cm.nipy_spectral,
+    ax.scatter(X[:, 0], X[:, 1], X[:, 2], color=list(map(lambda item: color_func(bioms, item), range(len(X)))), cmap=plt.cm.nipy_spectral,
            edgecolor='k')
 
     ax.w_xaxis.set_ticklabels([])
@@ -61,6 +102,22 @@ def main():
     ax.w_zaxis.set_ticklabels([])
 
 
+    plt.show()
+    
+    pca = sklearn.decomposition.PCA(n_components=2)
+    pca.fit(features)
+    X = pca.transform(features)
+    
+    fig2 = plt.figure(2, figsize=(4,3))
+    plt.clf()
+    ax2 = fig2.add_subplot(111)
+    plt.cla()
+    ax2.scatter(X[:, 0], X[:, 1], color=list(map(lambda item: color_func(bioms, item), range(len(X)))), cmap=plt.cm.nipy_spectral,
+           edgecolor='k')
+    
+    ax2.set_yticklabels([])
+    ax2.set_xticklabels([])
+    
     plt.show()
 
 if __name__ == "__main__":
